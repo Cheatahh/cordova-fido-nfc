eaa2110379db913758edf6eb2db89b2b6f613e49
Cheatahh <florian.stachnet@gmail.com>
2026-01-20 10:25:18 +0100

    3

--- src/android/FidoWebview.kt
+++ src/android/FidoWebview.kt
@@ -2,6 +2,7 @@ package com.fkmit.fido
 
 import org.apache.cordova.CallbackContext
 import org.apache.cordova.CordovaPlugin
+import org.apache.cordova.PluginResult
 import org.json.JSONArray
 import org.json.JSONObject
 
